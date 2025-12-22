b5efad6eb5c34c6b30d9d3af32d949e8b25c220d
Baryshnikov Egor Alexandrovich <eabaryshnikov@mai.education>
2025-12-22 18:33:13 +0300

    add BookCollection

--- src/book.py
+++ src/book.py
@@ -14,3 +14,23 @@ class Book:
         self.genre = genre
         self.isbn = isbn
 
+
+class BookCollection:
+    def __init__(self) -> None:
+        self._books: list[Book] = []
+    
+    def __getitem__(self, key: int | slice) -> Book | list[Book]:
+        return self._books[key]
+    
+    def __iter__(self):
+        return iter(self._books)
+    
+    def __len__(self) -> int:
+        return len(self._books)
+    
+    def add(self, book: Book) -> None:
+        self._books.append(book)
+    
+    def remove(self, book: Book) -> None:
+        self._books.remove(book)
+
