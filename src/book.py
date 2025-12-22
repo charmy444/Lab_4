25657cd738ee34394f0f3b68eb11e9e303ecbb91
Baryshnikov Egor Alexandrovich <eabaryshnikov@mai.education>
2025-12-22 18:48:25 +0300

    add BookIndex

--- src/book.py
+++ src/book.py
@@ -17,20 +17,71 @@ class Book:
 
 class BookCollection:
     def __init__(self) -> None:
-        self._books: list[Book] = []
+        self.books: list[Book] = []
     
     def __getitem__(self, key: int | slice) -> Book | list[Book]:
-        return self._books[key]
+        return self.books[key]
     
     def __iter__(self):
-        return iter(self._books)
+        return iter(self.books)
     
     def __len__(self) -> int:
-        return len(self._books)
+        return len(self.books)
     
     def add(self, book: Book) -> None:
-        self._books.append(book)
+        self.books.append(book)
     
     def remove(self, book: Book) -> None:
-        self._books.remove(book)
+        self.books.remove(book)
+
+
+class BookIndex:
+    def __init__(self) -> None:
+        self.isbn_index: dict[str, Book] = {}
+        self.author_index: dict[str, list[Book]] = {}
+        self.year_index: dict[int, list[Book]] = {}
+    
+    def __getitem__(self, key: str | int) -> Book | list[Book]:
+        if isinstance(key, str):
+            if key in self.isbn_index:
+                return self.isbn_index[key]
+            if key in self.author_index:
+                return self.author_index[key]
+        if isinstance(key, int):
+            if key in self.year_index:
+                return self.year_index[key]
+        raise KeyError(key)
+    
+    def __iter__(self):
+        return iter(self.isbn_index.values())
+    
+    def __len__(self) -> int:
+        return len(self.isbn_index)
+    
+    def add(self, book: Book) -> None:
+        self.isbn_index[book.isbn] = book
+        
+        if book.author not in self.author_index:
+            self.author_index[book.author] = []
+        self.author_index[book.author].append(book)
+        
+        if book.year not in self.year_index:
+            self.year_index[book.year] = []
+        self.year_index[book.year].append(book)
+    
+    def remove(self, book: Book) -> None:
+        if book.isbn not in self.isbn_index:
+            raise KeyError(f"Книга с ISBN {book.isbn} не найдена")
+        
+        del self.isbn_index[book.isbn]
+        
+        if book.author in self.author_index:
+            self.author_index[book.author].remove(book)
+            if not self.author_index[book.author]:
+                del self.author_index[book.author]
+        
+        if book.year in self.year_index:
+            self.year_index[book.year].remove(book)
+            if not self.year_index[book.year]:
+                del self.year_index[book.year]
 
